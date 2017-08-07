939e01f1d243f592c3dc07d37c6b3303a5a7e971
Giedrius Masalskis <giedrius.masalskis@softra.lt>
2017-08-07 10:28:13 +0300

    Select2 version rolled back to 3.2

--- assets/javascripts/redmine_improved_searchbox.js
+++ assets/javascripts/redmine_improved_searchbox.js
@@ -1,10 +1,11 @@
 $(function(){
-    if ($(window).width() < 900) 
-        return;
+// Disabled, because it breaks select2 on Chromium based browsers.
+//  if ('ontouchstart' in window || !!(navigator.msMaxTouchPoints)) return;
+  if ($( window ).width() < 900) return;
 
-	var str = $('#project_quick_jump_box option:first').text();
-	$('#project_quick_jump_box option:first').text("").val("");
-	$('#project_quick_jump_box').attr("data-placeholder", str).select2({ width: function() {
-		return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
-	}});
+  var str = $('#project_quick_jump_box option:first').text();
+  $('#project_quick_jump_box option:first').text("").val("");
+  $('#project_quick_jump_box').attr("data-placeholder", str).select2({ width: function() {
+    return ($('#project_quick_jump_box').width() + 32).toString() + 'px';
+  }});
 });
